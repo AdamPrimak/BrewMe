328a9daa8489be3d2cd0202e3a606f8092c49468
jerrydu5 <dum@rpi.edu>
2019-10-30 01:43:10 -0400

    Added code for LED's and get actual Centigrate and Fahrenheit values from the input and change the interval to 60s

--- BrewMe.py
+++ BrewMe.py
@@ -1,6 +1,23 @@
+#Setup
 import glob
 import time
- 
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT) #Green LED
+GPIO.setup(12,GPIO.OUT) #RED LED
+GPIO.setup(13,GPIO.OUT) #Yellow LED
+
+
+print ("LED on") #How to turn them on
+GPIO.output(18,GPIO.HIGH)
+GPIO.output(12,GPIO.HIGH)
+GPIO.output(13,GPIO.HIGH)
+time.sleep(1)# Time on duration
+print ("LED off") #How to turn them off
+GPIO.output(18,GPIO.LOW)
+GPIO.output(12,GPIO.LOW)
+GPIO.output(13,GPIO.LOW)
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
@@ -19,8 +36,10 @@ def read_temp():
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
-        return float(temp_string)
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
  
 while True:
     print(read_temp())
-    time.sleep(1)
+    time.sleep(60)
