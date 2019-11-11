3d123a1dda5dfde3d6e983bf00c4025c457e77b0
jerrydu5 <dum@rpi.edu>
2019-11-11 17:10:18 -0500

    added code for threshold checks

--- BrewMe.py
+++ BrewMe.py
@@ -9,19 +9,11 @@ GPIO.setup(12,GPIO.OUT) #RED LED
 GPIO.setup(13,GPIO.OUT) #Yellow LED
 
 
-print ("LED on") #How to turn them on
-GPIO.output(18,GPIO.HIGH)
-GPIO.output(12,GPIO.HIGH)
-GPIO.output(13,GPIO.HIGH)
-time.sleep(1)# Time on duration
-print ("LED off") #How to turn them off
-GPIO.output(18,GPIO.LOW)
-GPIO.output(12,GPIO.LOW)
-GPIO.output(13,GPIO.LOW)
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
- 
+
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
@@ -39,7 +31,28 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
+    
+def LED_color(): #hard coded limits on temperature - Green = within limits, Yellow = near limits, Red = outside limits
+    temp = read_temp()
+    if temp[0] < 5 or temp[0] > 30:
+        GPIO.output(18,GPIO.LOW)
+        GPIO.output(13,GPIO.LOW)
+        GPIO.output(12,GPIO.HIGH)
+        return "Temprature is outside of limits"
+    if temp[0] < 15 and temp[0] > 5 or temp[0] < 30 and temp[0] > 20:
+        GPIO.output(12,GPIO.LOW)
+        GPIO.output(18,GPIO.LOW)
+        GPIO.output(13,GPIO.HIGH)
+        return "Temperature is near the limits"
+    if temp[0] < 20 and temp[0] > 15:
+        GPIO.output(18,GPIO.HIGH)
+        GPIO.output(12,GPIO.LOW)
+        GPIO.output(13,GPIO.LOW)
+        return "Temperature is within the range"
+    
  
 while True:
+    temp = 0
     print(read_temp())
-    time.sleep(60)
+    print(LED_color())
+    time.sleep(1)
