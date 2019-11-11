5e49709b3167ef9dba573d1d523af54415f01210
AdamPrimak <adprimak@gmail.com>
2019-11-11 17:40:07 -0500

    testing BT sockets on raspberry pi

---
+++ BTConnect.py
@@ -0,0 +1,12 @@
+import socket
+
+serverMACAddress = 'F8:E6:1A:0C:B5:89'
+port = 3
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((serverMACAddress,port))
+while 1:
+    text = input()
+    if text == "quit":
+        break
+    s.send(bytes(text, 'UTF-8'))
+s.close()
